6fc389b67390526b8d1a4a5b57833cb5c81bbf5b
Jonathan Lindquist <jonathan.lindquist17@gmail.com>
2020-06-10 19:12:27 -0500

    adding navigation components & Logo

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 
 const INGREDIENT_PRICES = {
 	salad: 0.5,
@@ -28,7 +30,8 @@ class BurgerBuilder extends Component {
 			meat: 0
 		},
 		totalPrice: 4,
-		purchaseable: false
+		purchaseable: false,
+		purchasing: false
 	};
 
 	updatePurchaseState = (ingredients) => {
@@ -44,7 +47,7 @@ class BurgerBuilder extends Component {
 				return sum + el;
 			}, 0);
 
-		this.setState({purchaseable: sum > 0});
+		this.setState({ purchaseable: sum > 0 });
 	};
 
 	addIngredientHandler = (type) => {
@@ -64,9 +67,9 @@ class BurgerBuilder extends Component {
 		this.setState({
 			ingredients: updatedIngredients,
 			totalPrice: newPrice
-        });
+		});
 
-        this.updatePurchaseState(updatedIngredients);
+		this.updatePurchaseState(updatedIngredients);
 	};
 
 	removeIngredientHandler = (type) => {
@@ -89,9 +92,21 @@ class BurgerBuilder extends Component {
 		this.setState({
 			ingredients: updatedIngredients,
 			totalPrice: newPrice
-        });
-        
-        this.updatePurchaseState(updatedIngredients);
+		});
+
+		this.updatePurchaseState(updatedIngredients);
+	};
+
+	purchaseHandler = () => {
+		this.setState({ purchasing: true });
+	};
+
+	purchaseCancelhandler = () => {
+		this.setState({ purchasing: false });
+	};
+
+	purchaseContinueHandler = () => {
+		alert('You Continue!');
 	};
 
 	render() {
@@ -104,6 +119,14 @@ class BurgerBuilder extends Component {
 
 		return (
 			<Aux>
+				<Modal show={this.state.purchasing} modalClosed={this.purchaseCancelhandler}>
+					<OrderSummary
+						ingredients={this.state.ingredients}
+						purchaseCanceled={this.purchaseCancelhandler}
+						purchaseContinued={this.purchaseContinueHandler}
+						price={this.state.totalPrice}
+					/>
+				</Modal>
 				<div>
 					<Burger ingredients={this.state.ingredients} />
 				</div>
@@ -111,8 +134,9 @@ class BurgerBuilder extends Component {
 					disabled={disabledInfo}
 					ingredientAdded={this.addIngredientHandler}
 					ingredientRemoved={this.removeIngredientHandler}
-                    price={this.state.totalPrice}
-                    purchaseable={this.state.purchaseable}
+					price={this.state.totalPrice}
+					purchaseable={this.state.purchaseable}
+					ordered={this.purchaseHandler}
 				/>
 			</Aux>
 		);
