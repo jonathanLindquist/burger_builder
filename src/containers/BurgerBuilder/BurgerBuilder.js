ad7f66790b7365855ffca696ef21b60970f734f5
Jonathan Lindquist <jonathan.lindquist17@gmail.com>
2020-06-22 18:02:28 -0500

    formatting

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -35,15 +35,18 @@ class BurgerBuilder extends Component {
 	};
 
 	componentDidMount() {
-		axios.get('https://burger-builder-backend-159db.firebaseio.com/ingredients.json').then((response) => {
-			this.setState({
-				ingredients: response.data
-			});
-		}).catch(error => {
-			this.setState({
-				error: true
+		axios
+			.get('https://burger-builder-backend-159db.firebaseio.com/ingredients.json')
+			.then((response) => {
+				this.setState({
+					ingredients: response.data
+				});
 			})
-		});
+			.catch((error) => {
+				this.setState({
+					error: true
+				});
+			});
 	}
 
 	updatePurchaseState = (ingredients) => {
@@ -158,6 +161,7 @@ class BurgerBuilder extends Component {
 		const disabledInfo = {
 			...this.state.ingredients
 		};
+
 		for (let key in disabledInfo) {
 			disabledInfo[key] = disabledInfo[key] <= 0;
 		}
